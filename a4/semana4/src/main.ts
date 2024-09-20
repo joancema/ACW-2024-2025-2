193ffbdf80663b504bf656cf7c87431e0930c718
John Cevallos <joancemac@gmail.com>
2024-09-20 16:02:40 -0500

    Refactor form creation and voting functionality in main.ts

--- a4/semana4/src/main.ts
+++ a4/semana4/src/main.ts
@@ -37,26 +37,99 @@ votos.forEach((voto: IVoto) => {
 })
 
 
-divcontenido.innerHTML += `
-  <form id="formvoto">
-    <label for="ciudadano">Ciudadano</label>
-    <select name="ciudadano" id="ciudadano">
-      <option value="0">Ninguno</option>
-    </select>
-    <label for="pregunta">Pregunta</label>
-    <select name="pregunta" id="pregunta">
-      <option value="0">Ninguno</option>
-    </select>
-    <label for="voto">Voto</label>
-    <select name="voto" id="voto">
-      <option value="SI">SI</option>
-      <option value="NO">NO</option>
-    </select>
-    <button type="submit">Votar</button>
-  </form>
+const frmvoto = document.createElement('form')
+frmvoto.innerHTML = `
+  <label for="ciudadano">Ciudadano</label>
+  <select name="ciudadano" id="ciudadano">
+    <option value="0">Ninguno</option>
+  </select>
+  <label for="pregunta">Pregunta</label>
+  <select name="pregunta" id="pregunta">
+    <option value="0">Ninguno</option>
+  </select>
+  <label for="voto">Voto</label>
+  <select name="voto" id="voto">
+    <option value="SI">SI</option>
+    <option value="NO">NO</option>
+  </select>
+  <button type="submit">Votar</button>
 `
 
-const formvoto = document.querySelector<HTMLFormElement>('#formvoto')!
+divcontenido.appendChild(frmvoto)
+
+
+// <select>
+//   <option value="1">opcion1</option>
+//   <option value="2">opcion2</option>
+//   <option = value="3">opcion3</option>
+// </select>
+
+
+ciudadanos.forEach((ciudadano: ICiudadano) => {
+  const option = document.createElement('option')
+  option.value = ciudadano.id.toString()
+  option.textContent = ciudadano.nombre
+  // document.querySelector<HTMLSelectElement>('#ciudadano')!.appendChild(option)
+  frmvoto.ciudadano.appendChild(option)
+})
+
+frmvoto.pregunta.innerHTML += preguntas.map((pregunta) => 
+  `<option value="${pregunta.id}">${pregunta.pregunta}</option>`
+).join('')
+
+frmvoto.addEventListener('submit', (e) => {
+  e.preventDefault()
+  console.log(e.target)
+  const idCiudadano = frmvoto.ciudadano.value;
+  const idPregunta = frmvoto.pregunta.value;
+  const voto = frmvoto.voto.value;
+  console.log(idCiudadano, idPregunta, voto)
+  const ciudadano = ciudadanos.find((ciudadano) => ciudadano.id === parseInt(idCiudadano))!;
+  const pregunta = preguntas.find((pregunta) => pregunta.id === parseInt(idPregunta))!;
+  const id = votos.length + 1;
+  const votoNuevo = { id, ciudadano, pregunta, voto }
+  votos.push(votoNuevo)
+  const tr = document.createElement('tr')
+  tr.innerHTML = `
+    <td>${votoNuevo.id}</td>
+    <td>${votoNuevo.ciudadano.nombre}</td>
+    <td>${votoNuevo.pregunta.pregunta}</td>
+    <td>${votoNuevo.voto}</td>
+    <td>
+      <button>Editar</button>
+      <button>Eliminar</button>
+    </td>
+  `
+  tabla.appendChild(tr)
+})
+
+
+tabla.addEventListener('click', (e) => {
+  if (!(e.target instanceof HTMLButtonElement)){
+    return;
+  }
+  const boton = e.target;
+  if ( boton.textContent === 'Editar'){
+    const tr = boton.parentElement?.parentElement;
+    const id =  parseInt(tr?.firstElementChild?.textContent || '0');
+    const voto = votos.find((voto) => voto.id === id);
+    frmvoto.ciudadano.value = voto?.ciudadano.id.toString();
+    frmvoto.pregunta.value = voto?.pregunta.id.toString();
+    frmvoto.voto.value = voto?.voto;
+  } 
+  if ( boton.textContent === 'Eliminar'){
+    const tr = boton.parentElement?.parentElement;
+    const id = parseInt(tr?.firstElementChild?.textContent || '0');
+    const index = votos.findIndex((voto) => voto.id === id);
+    votos.splice(index, 1);
+    console.log(votos)
+    // const votos = votos.filter((voto) => voto.id !== id);
+    tr?.remove();
+  }
+})
+
+
+
 
 
 
