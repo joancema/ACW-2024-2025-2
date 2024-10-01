f6a2c6eeda5b0e950447532205481e77a146b919
John Cevallos <joancemac@gmail.com>
2024-09-30 21:16:21 -0500

    Refactor user component to display users in a table and allow adding, deleting, and editing users

--- W6/proyecto/ejemplo2/src/app/pages/user/user.component.ts
+++ W6/proyecto/ejemplo2/src/app/pages/user/user.component.ts
@@ -1,22 +1,34 @@
 import { Component } from '@angular/core';
 import { UsersTsService } from '../../services/users.ts.service';
 import { IUser } from '../../interfaces/IUser';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [FormsModule ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
 
   users:IUser[]=[]
+  user:IUser={  name: '', email: '', age: 0, active: false};
 
   constructor(private userService: UsersTsService  ) { }
 
   ngOnInit(): void {
     this.users = this.userService.getUsers();
+  }
+  addUser(){
+    this.userService.addUser(this.user);
+    this.user={  name: '', email: '', age: 0, active: false};
+  }
+  deleteUser(user:IUser){
+    this.userService.deleteUser(user);
+  }
+  selectUser(user:IUser){
+    this.user=user;
   }
 
 
--- W6/proyecto/ejemplo2/src/app/services/users.ts.service.ts
+++ W6/proyecto/ejemplo2/src/app/services/users.ts.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@angular/core';
 import { users } from '../data/users.data';
+import { IUser } from '../interfaces/IUser';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersTsService {
+  addUser(user: IUser) {
+    users.push(user);
+  }
+  deleteUser(user: IUser) {
+    users.splice(users.indexOf(user), 1);
+  }
 
   constructor() { }
   getUsers(){
