2c268d41f1ca03f4c5a55917edf4640ac5d5e3cf
John Cevallos <joancemac@gmail.com>
2024-10-05 07:17:09 -0500

    dadsfadf

--- W7/ejemplo1/src/app/pages/mostrar/mostrar.component.ts
+++ W7/ejemplo1/src/app/pages/mostrar/mostrar.component.ts
@@ -10,14 +10,15 @@ import { MostrarService } from '../../services/mostrar.service';
   templateUrl: './mostrar.component.html',
   styleUrl: './mostrar.component.css'
 })
+
 export class MostrarComponent {
 
   forma: FormGroup = new FormGroup({
-    nombre: new FormControl(''),
-    email: new FormControl(''),
-    password: new FormControl(''),
-    password2: new FormControl('')
+    code: new FormControl(''),
+    name: new FormControl(''),
   });
+
+  listado: IUser[] = [];
 
   constructor( private cliente: MostrarService  ) {
     console.log('servicio creado')
@@ -29,13 +30,39 @@ export class MostrarComponent {
 
   }
   guardar() {
-    console.log(this.forma.value);
-  }
-  consultar(){
-    console.log('consultar');
-    this.cliente.getAllDocuments().subscribe((data) => {
+    this.cliente.addDocument(this.forma.value).subscribe((data) => {
       console.log(data);
     })
   }
+  consultar(){
+    this.listado=[];
+    this.cliente.getAllDocuments().subscribe((data) => {
+      data.rows.forEach((element: any) => {
+        this.listado.push({
+          code: element.doc.code,
+          name: element.doc.name,
+          id: element.id,
+          rev: element.doc._rev
+        })
+      })
+    })
+  }
+  eliminar(elemento:IUser){
+    this.cliente.deleteDocument( elemento.id, elemento.rev ).subscribe((data) => {
+      console.log(data);
+    })
+  }
+  editar(elemento:IUser){
+    this.forma.setValue(elemento);
+    // this.cliente.updateDocument(this.forma.value).subscribe((data) => {
+      // console.log(data);
+    // })
+  }
 
+}
+interface IUser{
+  code: string;
+  name: string;
+  id: string;
+  rev: string;
 }
