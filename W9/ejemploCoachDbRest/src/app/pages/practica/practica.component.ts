ad2fc84f9618e76a29ef4883a874b877f8fad91a
John Cevallos <joancemac@gmail.com>
2024-10-28 14:07:59 -0500

    Refactor project structure and add user component

--- W9/ejemploCoachDbRest/src/app/pages/practica/practica.component.ts
+++ W9/ejemploCoachDbRest/src/app/pages/practica/practica.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { IUser } from '../../interfaces/IUser';
+import { PracticaService } from '../../services/practica.service';
 
 @Component({
   selector: 'app-practica',
@@ -12,6 +13,7 @@ import { IUser } from '../../interfaces/IUser';
   styleUrl: './practica.component.css'
 })
 export class PracticaComponent {
+
   forma: FormGroup= new FormGroup({
     code: new FormControl('001'),
     name: new FormControl('Prueba'),
@@ -23,6 +25,50 @@ export class PracticaComponent {
     rev: ""
   }
   users:IUser[] = [];
-  constructor() { }
+
+
+  constructor(private cliente: PracticaService  ) {
+
+
+  }
+
+
+  guardar(){
+
+    this.cliente.addDocument(this.forma.value).subscribe((data)=>{
+      console.log(data);
+    })
+
+  }
+  consultar(){
+    this.users=[];
+    this.cliente.getAlldocuments().subscribe((data)=>{
+      data.rows.forEach((element:any) => {
+        this.users.push({
+          id: element.doc._id,
+          name: element.doc.name,
+          code: element.doc.code,
+          rev: element.doc._rev
+        });
+      })
+    })
+  }
+  editar(elemento:IUser){
+    this.forma.controls['code'].setValue(elemento.code);
+    this.forma.controls['name'].setValue(elemento.name);
+    // this.forma.setValue(elemento);
+    // console.log(elemento);
+  }
+  eliminar(elemento:IUser){
+
+
+    this.cliente.deleteDocument(elemento.id, elemento.rev).subscribe((data)=>{
+      console.log(data);
+      this.consultar();
+    })
+
+
+
+  }
 
 }
