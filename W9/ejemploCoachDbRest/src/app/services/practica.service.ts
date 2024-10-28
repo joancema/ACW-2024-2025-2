0a98744ad8bf75f2c2d2c976803783cf4f9024d2
John Cevallos <joancemac@gmail.com>
2024-10-28 12:46:13 -0500

    Refactor project structure and add HttpClient to app.config.ts and PracticaService

--- W9/ejemploCoachDbRest/src/app/services/practica.service.ts
+++ W9/ejemploCoachDbRest/src/app/services/practica.service.ts
@@ -1,9 +1,45 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PracticaService {
 
-  constructor() { }
+  private couchDbUrl = 'http://localhost:5984/practica';
+
+
+  private getHttpOptions(){
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'Basic ' + btoa('admin:adminpassword')
+    })
+    return {headers};
+  }
+
+
+  constructor(private http: HttpClient) { }
+
+  addDocument(doc:any): Observable<any>
+  {
+    return this.http.post(this.couchDbUrl, doc, this.getHttpOptions());
+  }
+  getDocument(id:string ): Observable<any>
+  {
+    return this.http.get(`${this.couchDbUrl}/${id}`, this.getHttpOptions());
+  }
+  updateDocument( id:string, doc:any): Observable<any>
+  {
+    return this.http.put(`${this.couchDbUrl}/${id}`, doc, this.getHttpOptions());
+  }
+  deleteDocument( id:string, rev:string  ): Observable<any>
+  {
+    return this.http.delete(`${this.couchDbUrl}/${id}?rev=${rev}`, this.getHttpOptions());
+  }
+  getAlldocuments(): Observable<any>
+  {
+    return this.http.get(`${this.couchDbUrl}/_all_docs?include_docs=true`, this.getHttpOptions());
+  }
+
 }
