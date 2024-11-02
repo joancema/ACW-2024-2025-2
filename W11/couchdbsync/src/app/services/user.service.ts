a359f3dab4ca5a8f6ff9a81df9dc2329c38d9272
John Cevallos <joancemac@gmail.com>
2024-11-02 14:28:35 -0500

    Add PouchDB integration to user service for document management and synchronization

--- W11/couchdbsync/src/app/services/user.service.ts
+++ W11/couchdbsync/src/app/services/user.service.ts
@@ -1,9 +1,54 @@
 import { Injectable } from '@angular/core';
 
+import PouchDB from 'pouchdb';
+import find from 'pouchdb-find';
+
+
+PouchDB.plugin(find);
+
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private _db:any;
+
+  constructor() {
+    this._db = new PouchDB('customers');
+  }
+  addDocument(doc:any){
+    return this._db.put(doc);
+  }
+  getDocument(id:string){
+    return this._db.get(id);
+  }
+  getAllDocuments(){
+    return this._db.allDocs({include_docs:true});
+  }
+  updateDocument(doc:any){
+    return this._db.put(doc);
+  }
+  deleteDocument(doc:any){
+    return this._db.remove(doc);
+  }
+
+  syncWithCouchDB(remoteDbURL:string){
+    const remoteDb = new PouchDB(remoteDbURL);
+    this._db.sync(remoteDb, {
+      live: true,
+      retry: true
+    }).on('change', (change:any) => {
+      console.log('Change detected');
+    }).on('paused', (info:any) => {
+      console.log('Replication paused');
+    }).on('active', (info:any) => {
+      console.log('Replication resumed');
+    }).on('error', (err:any) => {
+      console.log('Sync error');
+    });
+  }
+
+
+
+
 }
