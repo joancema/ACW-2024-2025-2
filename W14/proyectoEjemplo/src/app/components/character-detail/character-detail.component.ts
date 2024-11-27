1af7dda480644d41eee7546fd65dcebb21e90bc4
John Cevallos <joancemac@gmail.com>
2024-11-27 14:23:31 -0500

    Refactor character detail component to display character list and loading state

--- W14/proyectoEjemplo/src/app/components/character-detail/character-detail.component.ts
+++ W14/proyectoEjemplo/src/app/components/character-detail/character-detail.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
+import { Item } from '../../interfaces/ICharacters';
+import { CharacterService } from '../../services/character.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-character-detail',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './character-detail.component.html',
   styleUrl: './character-detail.component.css'
 })
 export class CharacterDetailComponent {
+  characters = input.required<Item[]>();
+  // characterService= inject(CharacterService);
 
 }
--- W14/proyectoEjemplo/src/app/pages/character/character.component.ts
+++ W14/proyectoEjemplo/src/app/pages/character/character.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CharactersService } from '../../services/characters.service';
+import { CharacterDetailComponent } from '../../components/character-detail/character-detail.component';
 
 @Component({
   selector: 'app-character',
   standalone: true,
-  imports: [],
+  imports: [ CharacterDetailComponent ],
   templateUrl: './character.component.html',
   styleUrl: './character.component.css'
 })
 export default class CharacterComponent {
+
+  charactersService =  inject(CharactersService);
+
+  get characters() {
+    return this.charactersService.charactersQuery;
+  }
+
 
 }
